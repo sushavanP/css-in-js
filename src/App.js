0385221d7111628df4eed76164b525b28d16c9f2
Sushavan Paul <sushavan.paul.524@gmail.com>
2020-08-30 20:28:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card } from "./components/card.component";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Card>
+        <div>I am a component</div>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card.component.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "./card.styles.css";
+
+export const Card = ({ children }) => <div className="card">{children}</div>;
